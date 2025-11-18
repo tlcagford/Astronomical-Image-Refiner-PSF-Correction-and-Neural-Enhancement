1c4f79c30d1c43c3df3f8afb7836bfca9a15eedf
Tony E Ford <tlcagford@gmail.com>
2025-11-18 13:49:34 -0700

    Update utils.py

--- nsm/utils.py
+++ nsm/utils.py
@@ -1,1 +1,10 @@
 from astropy.io import fits
+
+from pathlib import Path
+
+def read_fits_header(path):
+    hdr = fits.getheader(str(path))
+    return dict(hdr)
+
+def safe_path(p):
+    return Path(p).expanduser().resolve()
